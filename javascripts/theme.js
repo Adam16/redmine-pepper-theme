822b51fe526927ad2415306cdfa55064e5ca2ba9
Adam Balachowski <a.balachowski@opus-fashion.com>
2012-10-02 15:26:14 +0200

    move project selector to h1 element

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -3,5 +3,13 @@ $(document).ready(function () {
     $meta.name = 'viewport';
     $meta.content = 'width=450';
     $('head').append($meta);
+
+    var $select = $("#project_quick_jump_box").hide(),
+        $selectProject = $(document.createElement("div"));
+    $selectProject.addClass("projectSelector");
+    $selectProject.append($select);
+    $selectProject.insertAfter($("#header h1"));
+    $selectProject.show();
+    $select.show();
 });
 
