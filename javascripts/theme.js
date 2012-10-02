56c7d6e2503b96ceacbb3cb3e653c15299b18e3d
Adam Balachowski <a.balachowski@opus-fashion.com>
2012-10-02 14:37:23 +0200

    Change set innerHTML  to html() on dom element

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,9 +1,22 @@
+(function($) {
+  $.fn.down = function() {
+    var el = this[0] && this[0].firstChild;
+    while (el && el.nodeType != 1)
+      el = el.nextSibling;
+    return $(el);
+  };
+})(jQuery);
+
 function injectViewportMetaTag() {
   var meta = $(document.createElement('meta'));
   meta.name = 'viewport';
   meta.content = 'width=450';
   $('head')[0].append(meta);
 }
+
+var periodicalExecuter = function() {
+    setInterval("periodicalExecuter", 1000);
+};
 
 ProjectMenuBuilder = {
   buildMenuItem: function(project) {
@@ -55,12 +68,12 @@ ProjectMenuBuilder = {
   },
 
   getProjects: function(element) {
-    var projectOptions = element.getElementsBySelector('option[value!=""]');
-    return projectOptions.collect(function(node) {
+    var projectOptions = element.find('option[value!=""]');
+    return projectOptions.map(function(node) {
       return {
         url: node.value,
         name: node.innerHTML,
-        selected: node.readAttribute('selected') == 'selected'
+        selected: node.attr('selected') == 'selected'
       };
     });
   },
@@ -77,12 +90,12 @@ ProjectMenuBuilder = {
         this.hideProjects();
       } else {
         this.showProjects();
-      };
+      }
     };
 
     selector.hideProjects = function() {
       $(this).down('.projects').hide();
-      $(this).down('.toggle').removeClassName('active');
+      $(this).down('.toggle').removeClass('active');
     };
 
     selector.showProjects = function() {
@@ -98,10 +111,7 @@ ProjectMenuBuilder = {
 
     // Hide the dropdown again a short while after we've moved the mouse away
     selector.on('mouseout', function(event) {
-      selector.toggleTimer = new PeriodicalExecuter(function(pe) {
-        selector.hideProjects();
-        pe.stop();
-      }, 1);
+      selector.toggleTimer.hide();
     });
 
     // Cancel the timer to hide the dropdown if we move the mouse back over the menu
@@ -122,7 +132,7 @@ ProjectMenuBuilder = {
     ProjectMenuBuilder.bindEvents(selector);
 
     // Insert the project selector after the project name
-    projectName.insert({ after: selector });
+    projectName.insertAfter(selector);
 
     // Remove the original select list
     projectSelector.hide();
